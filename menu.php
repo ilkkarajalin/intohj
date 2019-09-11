56fb9ae36fca33c9f5bc50ee78d73aa3ebc092e0
ilkkarajalin <ilkka.rajalin@hotmail.fi>
2019-09-11 21:12:44 +0300

    html runko menulle

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+    <link rel="stylesheet" href="CSS/mystyle.css">
+  </head>
+  <body>
+    <div id="header">
+      This is the header
+    </div>
+    <div id="menu">
+      Menu
+    </div>
+    <div id="content">
+      This is the content
+    </div>
+    <div id="header">
+      This is the footer
+    </div>
+  </body>
+</html>
